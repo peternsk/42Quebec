9c4b5dd32beae34aafd117f47fc81a9335f8a41d
peternsk <petermalonge@gmail.com>
2023-02-03 13:28:03 -0500

    final

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pnsaka <pnsaka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 12:04:51 by pnsaka            #+#    #+#             */
-/*   Updated: 2023/02/02 17:00:19 by pnsaka           ###   ########.fr       */
+/*   Updated: 2023/02/03 13:08:59 by pnsaka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	ft_numchar(int c)
 {
 	ft_putchar_fd(c, 1);
 	return (1);
-} 
+}
 
 int	ft_prnt_strng(char *str)
 {
@@ -36,7 +36,7 @@ int	ft_prnt_strng(char *str)
 	return (i);
 }
 
-void	ft_putnbr_base_pf(int nb, char *base, int *len)
+void	ft_pnb_pf(long nb, char *base, int *len)
 {
 	size_t	base_lenght;
 
@@ -47,38 +47,35 @@ void	ft_putnbr_base_pf(int nb, char *base, int *len)
 		nb = nb * -1;
 	}
 	if (nb / base_lenght != 0)
-		ft_putnbr_base_pf(nb / base_lenght, base, len);
+		ft_pnb_pf(nb / base_lenght, base, len);
 	*len += ft_numchar(base[nb % base_lenght]);
 }
 
 int	ft_addarg(va_list args, char format_specifier)
 {
-	int sum_char;
-	
-	sum_char = 0;
+	int	sum;
+
+	sum = 0;
 	if (format_specifier == 'c')
-		sum_char += ft_numchar(va_arg(args, int));
+		sum += ft_numchar(va_arg(args, int));
 	else if (format_specifier == 's')
-		sum_char += ft_prnt_strng(va_arg(args, char *));
+		sum += ft_prnt_strng(va_arg(args, char *));
 	else if (format_specifier == 'd' || format_specifier == 'i')
-		ft_putnbr_base_pf(va_arg(args, int), "0123456789", &sum_char);
+		ft_pnb_pf(va_arg(args, int), "0123456789", &sum);
 	else if (format_specifier == 'x')
-		ft_putnbr_base_pf(va_arg(args, unsigned int), "0123456789abcdef",
-			&sum_char);
+		ft_pnb_pf(va_arg(args, unsigned int), "0123456789abcdef", &sum);
 	else if (format_specifier == 'X')
-		ft_putnbr_base_pf(va_arg(args, unsigned int), "0123456789ABCDEF",
-			&sum_char);
+		ft_pnb_pf(va_arg(args, unsigned int), "0123456789ABCDEF", &sum);
 	else if (format_specifier == 'u')
-		ft_putnbr_base_pf(va_arg(args, unsigned long), "0123456789", &sum_char);
+		ft_pnb_pf(va_arg(args, unsigned int), "0123456789", &sum);
 	else if (format_specifier == '%')
-		sum_char += ft_numchar('%');
+		sum += ft_numchar('%');
 	else if (format_specifier == 'p')
 	{
-		sum_char += ft_prnt_strng("0x");
-		ft_putnbr_base_pf(va_arg(args, unsigned long), "0123456789abcdef",
-			&sum_char);
+		sum += ft_prnt_strng("0x");
+		ft_pnb_pf(va_arg(args, unsigned long), "0123456789abcdef", &sum);
 	}
-	return (sum_char);
+	return (sum);
 }
 
 int	ft_printf(const char *str, ...)
