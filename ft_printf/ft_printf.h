25f3b6b1fd9197f20525b4b0020f8e4a9d1f45b5
peternsk <petermalonge@gmail.com>
2023-02-10 15:58:29 -0500

    get, print makef

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: pnsaka <pnsaka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 12:03:58 by pnsaka            #+#    #+#             */
-/*   Updated: 2023/02/03 13:09:16 by pnsaka           ###   ########.fr       */
+/*   Updated: 2023/02/10 14:25:42 by pnsaka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,6 @@
 # include "libft/libft.h"
 # include <stdarg.h>
 
-int		ft_numchar(int c);
-int		ft_prnt_strng(char *str);
-void	ft_pnb_pf(long nb, char *base, int *len);
-int		ft_addarg(va_list args, char format_specifier);
 int		ft_printf(const char *str, ...);
 
 #endif
